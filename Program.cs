81cd7b6982a0527ba43b20827cb6a8aea32ef85a
unknown <srikant.mar1984@outlook.com>
2020-03-30 01:37:24 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace core_dev_api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
